43abe13e53ea2290d9f8b0ad0757a389ac1a3787
Siddharth Ghosh <email>
2023-08-27 11:58:58 +0530

    Expression Validation Added, More Mathematical & Bitwise Operatiions Added

--- App.js
+++ App.js
@@ -17,25 +17,62 @@ function expressionFunction(arrayTextExpression) {
     Minus: "-",
     Into: "*",
     By: "/",
+    Remainder: "%",
+    RaisedTo: "**",
+    AND: "&",
+    OR: "|",
+    XOR: "^",
+    LeftBracket: "(",
+    RightBracket: ")",
   };
   return arrayTextExpression
     .map((element) => operatorList[element] || element)
     .join("");
 }
 
-App.get("/:Expression*", (req, res) => {
+let Result = 0;
+
+App.get("/BitwiseOperations/:Expression*", (req, res) => {
   const originalURL = req.originalUrl;
-
-  const textExpression = originalURL.slice(1);
-
+  const textExpression = originalURL.split("/BitwiseOperations/")[1];
   const arrayTextExpression = textExpression.split("/");
 
+  if (arrayTextExpression.length < 3) {
+    res.json({
+      Problem: "Expression Length Short",
+    });
+  }
+
   const actualExpression = expressionFunction(arrayTextExpression);
+  const regexExpression = /^[0-9&|^()]*$/;
+
+  if (!regexExpression.test(actualExpression)) {
+    res.json({ Problem: "Invalid Operator/Number" });
+  }
+
+  const Result = eval(actualExpression);
+
+  res.json({ Expression: actualExpression, Answer: Result });
+});
+
+App.get("/:Expression*", (req, res) => {
+  const originalURL = req.originalUrl;
+  const textExpression = originalURL.slice(1);
+  const arrayTextExpression = textExpression.split("/");
+
+  if (arrayTextExpression.length < 3) {
+    res.json({ Problem: "Expression Length Short" });
+  }
+
+  const actualExpression = expressionFunction(arrayTextExpression);
+  const regexExpression = /^[0-9()+\-*/%.]*$/;
+
+  if (!regexExpression.test(actualExpression)) {
+    res.json({ Problem: "Invalid Operator/Number" });
+  }
 
   Result = eval(actualExpression);
-
   Result = Number(Result.toFixed(2));
-  console.log(Result);
 
   res.json({ Expression: actualExpression, Answer: Result });
 });
